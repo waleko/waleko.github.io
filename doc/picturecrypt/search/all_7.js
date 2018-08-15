7da74e534cdbe7944744b7733cc7a4e3fff21696
Alex Kovrigin <a.kovrigin0@gmail.com>
2018-08-15 12:53:33 +0300

    Some random changes

--- doc/picturecrypt/search/all_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['image',['image',['../class_encrypt_dialog.html#a739a0df1d28d06b28a3fd16e2bc16c73',1,'EncryptDialog']]],
-  ['inputfilename',['inputFileName',['../class_encrypt_dialog.html#a859b1bc2f032a247632b879bf8663d0b',1,'EncryptDialog']]],
-  ['iso',['ISO',['../class_q_a_e_s_encryption.html#ab0a65cdea4eac21ef32530010d1b0247a4fb686e6a16d4242ff35311d2e7c422d',1,'QAESEncryption']]]
-];
--- doc/picturecrypt/search/functions_d.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['viewpc',['ViewPC',['../class_view_p_c.html#acdef2e0a47bd6d85f4835391c79919eb',1,'ViewPC']]]
-];
